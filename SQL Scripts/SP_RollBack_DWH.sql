6e13eb0782b4dfed114e8e48aa1697374ffa6b9b
amit <amit@4b4139ef-f2f0-4a6a-8da1-e47a1fbc706f>
2012-09-10 14:45:09 +0000

    Update rollback dwh to support signature

--- SQL Scripts/SP_RollBack_DWH.sql
+++ SQL Scripts/SP_RollBack_DWH.sql
@@ -1,20 +1,29 @@
-USE [Edge_System291]
+USE [EdgeSystem]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_RollBack_DWH]   ****
-		Creator: Amit Bluman
-		Created date:  08/21/2012 16:28:55
-		Update date: --------
-		Update description: ----------
-**/
-
+/****** Object:  StoredProcedure [dbo].[SP_RollBack_DWH]    Script Date: 9/10/2012 4:48:06 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER procedure [dbo].[SP_RollBack_DWH] as
+ALTER procedure [dbo].[SP_RollBack_DWH] 
+( @DeliveryID nvarchar(4000),
+  @Signature nvarchar(4000), 
+  @CommitTableName nvarchar(4000)) AS
 
 -- This SP will find the "pending roll back" delivery outputs, delete it from the DWH db, and change it's status to rolledback.
 -- The SP belongs to system db
+
+-- Start debug
+/*
+	Declare @DeliveryID nvarchar(4000)
+	Declare @Signature nvarchar(4000)
+	Declare @CommitTableName nvarchar(4000)
+
+	-- Set @Signature = 'ZmFjZWJvb2stWzYxXS1bNTk2ODQ0MTFdLVt7c3RhcnQ6e2FsaWduOidTdGFydCcsZGF0ZTonMjAxMi0wOC0wOFQwMDowMDowMCd9LGVuZDp7YWxpZ246J0VuZCcsZGF0ZTonMjAxMi0wOC0wOFQyMzo1OTo1OS45OTk5OTk5J319XQ=='
+	Set @CommitTableName = 'Paid_API_Allcolumns'
+*/
+-- End debug 
+
 Declare @DebugMode as bit;
 Declare @OutputIDs as nvarchar(4000);
 Declare @TableNames as nvarchar(4000);
@@ -26,22 +35,44 @@ Declare @sql as nvarchar(4000);
 --	Drop table #OutputAndTable;
 --	Drop table #DistinctTables;
 	
-	set @DebugMode = 'False' ;
+	set @DebugMode = 'True' ;
 	set @DWHDB = 'EdgeDWH';
--- Find all the "pending roll back" statuses (7) in delivery output table. the data stored is table name and outputID
 
-	select DO.OutputID as OutputID, DOP.Value as TableName
-	into #OutputAndTable
-	from [dbo].[DeliveryOutput] DO	
-	  left outer join dbo.DeliveryOutputParameters DOP
-		on DO.DeliveryID = DOP.DeliveryID and DO.OutputID = DOP.OutputID
-	 where DO.Status = 7 and DOP.[Key] = 'CommitTableName';
-	 
+	Create table #OutputAndTable (OutputID Nvarchar(4000) not null, TableName Nvarchar(4000) not null)  ;
+
+-- The should ask for rollback per signature OR per commitTableName
+    If (@Signature is null and @CommitTableName is null)  Return;
+	
+	If (@Signature is not null and @CommitTableName is null) 	
+-- Find all the "pending roll back" statuses (7) in delivery output table for the specific signature. the data stored is table name and outputID
+	Begin
+		Insert into #OutputAndTable
+		select DO.OutputID as OutputID, DOP.Value as TableName
+		from [dbo].[DeliveryOutput] DO	
+		  left outer join dbo.DeliveryOutputParameters DOP
+			on DO.DeliveryID = DOP.DeliveryID and DO.OutputID = DOP.OutputID
+		where  DO.[Signature] = @Signature and DO.Status = 7 and DOP.[Key] = 'CommitTableName';
+	End
+	
+	
+	If (@Signature is null and @CommitTableName is not null) 	
+-- Find all the "pending roll back" statuses (7) in delivery output table for the specific commitTableName. the data stored is table name and outputID
+	Begin
+		Insert into #OutputAndTable
+		select DO.OutputID as OutputID, DOP.Value as TableName
+		from [dbo].[DeliveryOutput] DO	
+		  left outer join dbo.DeliveryOutputParameters DOP
+			on DO.DeliveryID = DOP.DeliveryID and DO.OutputID = DOP.OutputID
+		where  DO.Status = 7 and DOP.[Key] = 'CommitTableName' and DOP.Value = @CommitTableName;
+	End
+
+-- Distinct the table names	 
+	
 	Select distinct TableName
 	into #DistinctTables
-	from #OutputAndTable
+	from #OutputAndTable 
 
- -- Populate the table names list		
+-- Populate the table names list		
 	select @TableNames = COALESCE(@TableNames+'', '','''')+ ISNULL(TableName+',','''')
 	from  #DistinctTables;
 	
