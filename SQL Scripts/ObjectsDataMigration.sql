d4d4d7629b7772eef6912450c89918f4e238465c
amit <amit@4b4139ef-f2f0-4a6a-8da1-e47a1fbc706f>
2012-09-24 16:20:19 +0000

    Add ObjectsDataMigration.sql

---
+++ SQL Scripts/ObjectsDataMigration.sql
@@ -0,0 +1,16 @@
+﻿
+-- Need to filter the migration data by account as an option
+-- Create Client hirerchy
+Insert into EdgeObjects.dbo.Account
+Select distinct Client_ID as ID, Client_Name as Name, 
+	NULL, NULL as ParentAccountID
+From EDGE_OLTP_OLD.dbo.User_GUI_Account
+
+-- Create Account hirerchy
+Insert into EdgeObjects.dbo.Account
+Select Account_id as ID, Account_Name as Name, 
+	 Client_ID as ParentAccountID,
+	Case when [Status] = 0 then 0
+		Else 1
+	End as [Status]
+From EDGE_OLTP_OLD.dbo.User_GUI_Account
