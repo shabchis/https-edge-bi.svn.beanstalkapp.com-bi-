28e45e40d3a0dc23a5c00e6f9dea180e09e914fc
amit <amit@4b4139ef-f2f0-4a6a-8da1-e47a1fbc706f>
2014-03-17 15:06:46 +0000

    march 17 2014 adding sitelink to transform

---
+++ SQL Scripts/Insert_KW.sql
@@ -0,0 +1,18 @@
+
+
+  SET IDENTITY_INSERT [dbo].[UserProcess_GUI_PaidAdgroupKeyword] ON
+
+  INSERT INTO seperia.[dbo].[UserProcess_GUI_PaidAdgroupKeyword]
+           (ppc_keyword_gk, [Account_ID] ,[kwSite] ,[Channel_ID] ,[Keyword_GK],[Gateway_GK] ,[AdGroup_GK],[Campaign_GK] ,[LastUpdated],[MatchType])
+VALUES (-124027180,1240272,'No Keyword Data',20,-1240272,-124027180,-124027180,-124027180,getdate(),0 )
+ --VALUES (-124023486, 1240235,'No Keyword Data',14,-124023486,-124023486,-124023486,-124023486,getdate(),0 )
+--VALUES (-124023474, 1240235,'No Keyword Data',26,-124023474,-124023474,-124023474,-124023474,getdate(),0 )
+--VALUES (-1003494,10035,'No Keyword Data',16,-1003494,-1003494,-1003494,-1003494,getdate(),0 )
+--VALUES (-123994,1240,'No Keyword Data',6,-123994,-123994,-123994,-123994,getdate(),0 )
+--VALUES (-100599,1006,'No Keyword Data',1,-100599,-100599,-100599,-100599,getdate(),0 )
+--VALUES (-100594,1006,'No Keyword Data',6,-100594,-100594,-100594,-100594,getdate(),0 )
+--VALUES (-100576,1006,'No Keyword Data',24,-100576,-100576,-100576,-100576,getdate(),0 )
+--VALUES (-698,7,'No Keyword Data',2,-698,-698,-698,-698,getdate(),0 )
+
+SET IDENTITY_INSERT [dbo].[UserProcess_GUI_PaidAdgroupKeyword] OFF
+
